628efe2f00c62d138eb3c70950dde7fec0068492
Kungrah <crazy_m0@live.nl>
2017-10-02 20:47:39 +0200

    fgf

---
+++ Test/Contact.php
@@ -0,0 +1,10 @@
+<p>dddd</p>
+
+<?php
+print_r($_POST);
+print_r($_GET);
+
+
+//$leraar = array ("paginanaam" => "koe");
+	echo $_GET['paginanaam'];
+?>
--- Test/index.php
+++ Test/index.php
@@ -27,31 +27,36 @@
 						<div class="col-md-12">
 							<h1> PHP opdrachten</h1>
 							<?php
-								if(isset($_POST['firstname'])) 
+								if(isset($_POST['firstname']) && !empty($_POST['firstname'])) {
 									$firstname = $_POST['firstname'];
-									else
-										$firstname = null;
+								} else {
+									$firstname = null;
+								}	
 
-								if(isset($_POST['lastname']))
+								if(isset($_POST['lastname']) && !empty($_POST['lastname'])) {
 									$lastname = $_POST['lastname'];
-									else
-										$lastname = null;
+								} else {
+									$lastname = null;
+								}
 
-								if(isset($_POST['email']))
+								if(isset($_POST['email']) && !empty($_POST['email'])) {
 									$email = $_POST['email'];
-									else
-										$email = null;
+								} else {
+									$email = null;
+								}
 
-								if(isset($_POST['re-email']))
+								if(isset($_POST['re-email']) && !empty($_POST['re-email'])) {
 									$reEmail = $_POST['re-email'];
-									else
-										$reEmail = null;
+								} else {
+									$reEmail = null;
+								}
 
-								if(isset($_POST['password']))
+								if(isset($_POST['password']) && !empty($_POST['password'])) {
 									$password = $_POST['password'];
-									else
-										$password = null;
-
+								} else {
+									$password = null;
+								}
+								
 							 ?>
 							<form action="" method="POST">
 								<label class="label-control" for="firstname"> First Name:</label>
@@ -67,8 +72,12 @@
 								<input type="submit" name="infoVerzenden" value="Sign Up"/>
 							</form>
 							<?php
-								if(isset($firstname) && isset($lastname) && isset($email) && isset($reEmail) && isset ($password)){
+								if(isset($_POST['infoVerzenden'])){
+									if($_POST['firstname'] && $_POST['lastname'] && $_POST['email']&& $_POST['re-email'] && $_POST['password']){
 									echo "het werk";
+								} else {
+									echo "Het werkt niet";
+								}
 								}
 							?>
 						</div>
@@ -76,7 +85,7 @@
 
 					<div class="row">
 						<div class="col-md-12">
-							<br/>
+							<br>
 							<h2>If-Statement</h2>
 							<form action"" method="POST">
 								<label Class="label-control" for="GeldRemco"> Voer geld in: </label>
@@ -89,7 +98,7 @@
 									$BankRemco = $_POST['BankRemco'];
 									if($BankRemco >= 1400)
 									{
-										echo "Remco heeft genoeg geld <br/>";
+										echo "Remco heeft genoeg geld <br>";
 										echo " Uw heeft: $BankRemco";
 									}
 									else
@@ -103,7 +112,7 @@
 
 					<div class="row">
 						<div class="col-md-12">
-							<br/>
+							<br>
 							<h2>If else</h2>
 							<form action="" method="POST">
 								<label class="label-control" for="Bank"> Bank: </label>
@@ -137,7 +146,7 @@
 
 					<div class="row">
 						<div class="col-md-12">
-							<br/>
+							<br>
 							<h2>Switch</h2>
 							<form action="" method="POST">
 								<label class="" for="Kleuren"> Kleuren:</label>
@@ -165,33 +174,134 @@
 							?>
 						</div>
 					</div>
-					<br/>
+					<br>
 
 					<div class="row">
 						<div class="col-md-12">
-							<h2>For Loop</h2><br/>
+							<h2>For Loop</h2><br>
 							<?php
-								for($variable = 1; $variable <= 10; $variable++){
- 									echo $variable ."<br/>";				
+								for($variable = 1; $variable <= 10; $variable++)
+								{
+ 									echo $variable ."<br>";				
 								}
 
-								 echo "<br/>";
 								 echo "<h2>Array uit lezen met een For loop</h2>";
-
 								$names = array('Glenn', 'Rens', 'Simone', 'Muhammed');
-									for($x = 0; $x <= count($names) -1; $x++){
-										echo $names[$x]. "<br/>";
+									for($x = 0; $x <= count($names) -1; $x++)
+									{
+										echo $names[$x]. "<br>";
 									}
 							?>
 						</div>
 					</div>
-					<br/>
+					<div class="row">
+						<div class="col-md-12">
+							<h2>For loop gebruiken met While Loop</h2><br>
+							<?php
+								$bankSaldo = 1;
+								while($bankSaldo <= 10)
+								{
+									$bankSaldo++;
+									echo "sucuk". "<br>";
+								}
+							?>
+
+							<br>
+							<h2>While Loop</h2>
+							<form action="" method="POST">
+								<label class="" for="saldo"></label>
+								<p>Begin saldo</p>
+								<input class="form-control" type="text" id="saldo1" name="saldo"/>
+								<p>Bedrag wat je wilt gaan sparen</p>
+								<input class="form-control" type="text" id="saldo2" name="saldoberekening"/>
+								<input type="submit" name="bedragverzenden" Value="Voer het bedrag in"/>
+							</form>
+							<?php
+								if(isset($_POST['saldo']) && isset($_POST['saldoberekening']))
+								{
+									$saldo = $_POST['saldo'];
+									$aantalMaanden = 0;
+									while($saldo <= $_POST['saldoberekening'])
+										{
+											$saldo = $saldo + 50;
+											$saldo = $saldo * 1.08;
+											$aantalMaanden++;
+										}
+										echo "Je moet " .$aantalMaanden . " maanden lang sparen";
+								}
+							?>
+						</div>
+					</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h2>Array</h2>
+								<?php
+								$inhoudDoosje = array("Rood","Geel","Groen","Blauw");
+									echo $inhoudDoosje[0]."<br>";
+									echo $inhoudDoosje[1]."<br>";
+									echo $inhoudDoosje[2]."<br>";
+									echo $inhoudDoosje[3]."<br>";
+								?>
+
+								<h3>Array uitlezen met een For Loop</h3>
+								<?php
+								for($n = 0; $n < sizeof ($inhoudDoosje); $n++)
+									{
+	 									echo $inhoudDoosje[$n] ."<br>";				
+									}
+								?>
+
+								<h3>Foreach</h3>
+								<?php
+								foreach ($inhoudDoosje as $val){
+									echo $val."<br>";
+								}
+								?>
+
+								<h3>Associatief Array</h3>
+								<?php
+								$persoon = array("Voornaam" => "Muhammed",
+												"Achternaam" => "Ozdemir",
+												"Plaatsnaam" => "Oss");
+
+									echo $persoon["Voornaam"]."<br>"; //Toont Kees op het scherm in plaats van Muhammed
+									echo $persoon["Achternaam"]."<br>"; //Toont Ozdemir op het scherm in plaats van Achternaam
+									echo $persoon["Plaatsnaam"]."<br>"; //Toont Oss in plaats van Plaatsnaam
+
+									echo "<br><h4>Foreach</h4>";
+									foreach ($persoon as $lab){
+										echo $lab."<br>";
+									}
+								?>
+								<h2>Multidimensionaal Array</h2>
+								<?php
+								$gegevens = array(array("Voornaam" => "Muhammed 1",
+														"Achternaam" => "Ozdemir 1",
+														"Plaatsnaam" => "Oss 1"),
+													array("Voornaam" => "Muhammed 2",
+														"Acternaam" => "Ozdemir 2",
+														"Plaatsnaam" => "Oss 2"),
+													array("Voornaam" => "Muhammed 3",
+														"Achternaam" => "Ozdemir 3",
+														"Plaatsnaam" => "Oss 3")
+												);
+									echo $gegevens[0]["Voornaam"]."<br>";
+									echo $gegevens[1]["Voornaam"]."<br>";
+									echo $gegevens[2]["Voornaam"]."<br>";
+
+									echo "<br><h3>Foreach</h3>";
+
+									foreach ($gegevens as $tes){
+										foreach ($tes as $pas){
+											echo $pas."<br>";
+										}
+									}
+								?>
+						</div>
+					</div>
+					<br>
 			<footer>
 			</footer>
-				<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-				</script>
-			    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
-			    </script>
 			    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous">
 			    </script>
 		</body>
